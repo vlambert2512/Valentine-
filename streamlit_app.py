e9028a0baf3a09d6a276f9e955e957a37097e458
vlambert2512 <162982852+vlambert2512@users.noreply.github.com>
2024-03-18 11:58:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q
 st.dataframe(voc)
 l=voc.shape[0]
 i=np.random.choice(range(l))
-word_fr=voc['Définition'].value[i]
+word_fr=voc['Définition'].values[i]
 word_chi=voc['Hanzi'].values[i]
 st.write(word_fr+"Hanzi"+word_chi)
