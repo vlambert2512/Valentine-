6ac3f3f06149cde546d9b6919cd1b18f8d939a1f
vlambert2512 <162982852+vlambert2512@users.noreply.github.com>
2024-03-18 13:03:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+def is_correct(i,j):
+    if i==j:
+      st.write("Bravo")
+    else:
+      st.write("Raté")
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv')
 l=voc.shape[0]
 i=np.random.choice(range(l))
@@ -13,10 +18,6 @@ j=np.random.choice(indices)
 word_fr=voc["Définition"].values[j]
 st.write("Traduis: "+word_fr)
 for i in range(4):
-  st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
-def is_correct(i,j):
-    if i==j:
-      st.write("Bravo")
-    else:
-      st.write("Raté")
+  st.button(voc["Hanzi"].values[indices[i]], on_click=is_correct,args=(indices[i],j))
+
 
